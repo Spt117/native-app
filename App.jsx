da770d3659c3a38ec146214046d05b1caa4eae7f
jb <theshitcoinert@gmail.com>
2023-07-19 15:54:04 +0200

    test

---
+++ App.jsx
@@ -0,0 +1,5 @@
+import Human from "./components/Human";
+
+export default function App() {
+    return <Human />;
+}
---
+++ components/Human.jsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+
+export default function Human() {
+    return (
+        <>
+            <Text>Coucou JB ! Cool, ça marche !</Text>
+            <Text>Pas mal du tout !</Text>
+            <Text>Je suis content !</Text>
+        </>
+    );
+}
